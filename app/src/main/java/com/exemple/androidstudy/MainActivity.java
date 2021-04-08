4a893f9acdea43a110178f5fc2a35534d117db5d
roncan1 <carugame12_@gmail.com>
2021-04-08 18:05:33 +0900

    6 (SharedPreferences)

--- app/src/main/java/com/exemple/androidstudy/MainActivity.java
+++ app/src/main/java/com/exemple/androidstudy/MainActivity.java
@@ -3,6 +3,7 @@ package com.exemple.androidstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,8 +14,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_move; // 버튼 생성
-    EditText edit_test;
+    EditText edit_test,et_savetest;
     ImageView toast_img;
+    String save = "file";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +44,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        et_savetest = (EditText)findViewById(R.id.ET_savetest);
 
+        SharedPreferences sharedPreferences = getSharedPreferences(save, 0);
+        // SharedPreferences 불러오기 만들어둔 /String save = "file";/을 인자값으로 넣어줌
+        String value = sharedPreferences.getString("test", "");
+        // 저장된 값불러오기
+        et_savetest.setText(value);
+        //불러온 값 원래데로 세팅
+
+    }
+
+    @Override
+    protected void onDestroy() { // 액티비티가 파괴되었을때
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(save, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit(); //에디터 불러와서 연결
+        String value = et_savetest.getText().toString(); //et_savetest의 값을 문자열로 가져와 저장
+        editor.putString("test", value); // 에디터에 String형태로 test라는 이름으로 value값을 저장
+        editor.commit(); // 실제적으로 세이브를 완료
     }
 }
