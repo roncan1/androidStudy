3ff9ccf95c1c455a3e3257144f69644adc2d30dd
roncan <65841016+roncan1@users.noreply.github.com>
2021-04-24 23:18:15 +0900

    17 (구글맵 API)

--- app/src/main/java/com/exemple/androidstudy/MainActivity.java
+++ app/src/main/java/com/exemple/androidstudy/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+
+import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -21,7 +23,15 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     DrawerLayout drawerLayout;
     View drawerView;
@@ -33,18 +43,24 @@ public class MainActivity extends AppCompatActivity {
     Spinner spinner;
     long backBtnTime = 0;
     MediaPlayer mediaPlayer;
+    FragmentManager fragmentManager;
+    MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_music_play = (Button)findViewById(R.id.btn_music_play);
-        btn_music_stop = (Button)findViewById(R.id.btn_music_stop);
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+
+        btn_music_play = (Button) findViewById(R.id.btn_music_play);
+        btn_music_stop = (Button) findViewById(R.id.btn_music_stop);
 
         btn_music_play.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) { // 음악지정 후 재생
                 mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.music);
                 mediaPlayer.start();
             }
@@ -52,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
         btn_music_stop.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) { // 음악이 재생되있을떄 음악을 멈추고 리셋
                 if (mediaPlayer.isPlaying()) {
                     mediaPlayer.stop();
                     mediaPlayer.reset();
@@ -60,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        spinner = (Spinner)findViewById(R.id.spinner);
-        tv_spinner_result = (TextView)findViewById(R.id.tv_spinner_result);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        tv_spinner_result = (TextView) findViewById(R.id.tv_spinner_result);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() { // 무엇이 select 되었는진 확인
             @Override
@@ -76,8 +92,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        btn_ser_start = (Button)findViewById(R.id.btn_ser_start);
-        btn_ser_and = (Button)findViewById(R.id.btn_ser_and);
+        btn_ser_start = (Button) findViewById(R.id.btn_ser_start);
+        btn_ser_and = (Button) findViewById(R.id.btn_ser_and);
 
         btn_ser_start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,8 +111,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn_dialog = (Button)findViewById(R.id.btn_dialog);
-        tv_dia_result = (TextView)findViewById(R.id.tv_dia_result);
+        btn_dialog = (Button) findViewById(R.id.btn_dialog);
+        tv_dia_result = (TextView) findViewById(R.id.tv_dia_result);
 
         btn_dialog.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -131,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn_start = (Button)findViewById(R.id.btn_start);
+        btn_start = (Button) findViewById(R.id.btn_start);
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -139,22 +155,22 @@ public class MainActivity extends AppCompatActivity {
                 isThread = true; // 스레드가 실행되고있기에 값을 true로 변경
 
                 thread = new Thread() { // 쓰레드를 생성
-                  public void run() { // run이라는 메소드 생성
-                      while(isThread) { // while문으로 isThread가 true일 경우 반복
-                          try {
-                              sleep(5000);  // 5초에 한번씩 쓰레드가 실행되도록 함
-                          } catch (InterruptedException e) {
-                              e.printStackTrace();
-                          }                      // try / catch 로 예외처리를 해준다.
-                          handler.sendEmptyMessage(0); // 핸들러에 메세지를 전송
-                      }
-                  }
+                    public void run() { // run이라는 메소드 생성
+                        while (isThread) { // while문으로 isThread가 true일 경우 반복
+                            try {
+                                sleep(5000);  // 5초에 한번씩 쓰레드가 실행되도록 함
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }                      // try / catch 로 예외처리를 해준다.
+                            handler.sendEmptyMessage(0); // 핸들러에 메세지를 전송
+                        }
+                    }
                 };
                 thread.start(); // 스레드를 시작
             }
         });
 
-        btn_and = (Button)findViewById(R.id.btn_and);  // 스레드 종료
+        btn_and = (Button) findViewById(R.id.btn_and);  // 스레드 종료
         btn_and.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -163,12 +179,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         String logExem = "로그출력테스트";
-        Log.e("MainActivity : ",logExem); // 로그 테스트
+        Log.e("MainActivity : ", logExem); // 로그 테스트
 
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-        drawerView = (View)findViewById(R.id.drawer);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerView = (View) findViewById(R.id.drawer);
 
-        Button btn_open = (Button)findViewById(R.id.btn_open);
+        Button btn_open = (Button) findViewById(R.id.btn_open);
         btn_open.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -176,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn_close = (Button)findViewById(R.id.btn_close);
+        Button btn_close = (Button) findViewById(R.id.btn_close);
         btn_close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -193,7 +209,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
     }
 
     @Override
@@ -201,8 +216,7 @@ public class MainActivity extends AppCompatActivity {
         long curTime = System.currentTimeMillis();
         long gapTime = curTime - backBtnTime;
 
-        if (0 <= gapTime && 2000 >= gapTime)
-        {
+        if (0 <= gapTime && 2000 >= gapTime) {
             super.onBackPressed();
         } else {
             backBtnTime = curTime;
@@ -213,7 +227,7 @@ public class MainActivity extends AppCompatActivity {
     Handler handler = new Handler() {
         @Override
         public void handleMessage(@NonNull Message msg) {
-            Toast.makeText(getApplicationContext(), "핸들러 테스트",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "핸들러 테스트", Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -242,9 +256,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mediaPlayer != null) {
+
+        if (mediaPlayer != null) {// 중지를 안하고 앱을껏을떄 음악 초기화
             mediaPlayer.release();
             mediaPlayer = null;
         }
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng locarion = new LatLng(37.548610, 127.169223); //마커위치 (위도,경도)
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("상일미디어고등학교");
+        markerOptions.snippet("학교");
+        markerOptions.position(locarion);
+        googleMap.addMarker(markerOptions); //마커생성
+
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(locarion, 16)); //화면 세팅(줌 강도 : 16)
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(locarion, 16)); //2가지 방법
     }
 }
